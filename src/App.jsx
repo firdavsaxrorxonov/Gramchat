46365c4da1d0bcc49b93361016c82bb6813ea1ef
GPLEADER <axrorxonovfirdavs@gmail.com>
2025-06-13 12:08:02 +0500

    Faylar nastroyka qilinib qoyildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Home from './pages/Home';
 
-function App() {
+
+const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MainLayout>
+    </Router>
+  );
+};
 
-export default App
+export default App;
