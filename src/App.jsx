75ffaab621cbe628b107f767d0b8ac6e0fc60eed
Muhsinjon707 <foziljonovmuhsinjon@gmail.com>
2025-06-14 11:55:08 +0500

    add login & registration page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
+
+// Pages import
 import Home from './pages/Home';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
 
 
 const App = () => {
@@ -10,6 +14,8 @@ const App = () => {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </MainLayout>
     </Router>
